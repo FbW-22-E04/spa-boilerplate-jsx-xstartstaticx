e50f2722cf384885f0338550eb60e800b19925e5
xstartstaticx <daniel.reiller@icloud.com>
2022-10-10 14:44:16 +0200

    added name variable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
 
 // WHAT: This variable returns contains the html to render
-let output = (
-	<span>
-		James is 12 years old
-	</span>
-);
+let name = "James";
+let output = <span>{name} is 12 years old</span>;
 
 // WHERE: A DOM element that will contain the entire react generated html
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
 // create a root
 const root = ReactDOM.createRoot(container);
 
 //render app to root
 root.render(output);
-
-
-
-
